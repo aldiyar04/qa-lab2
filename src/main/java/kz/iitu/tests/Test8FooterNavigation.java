93a38d68a0e8149c0fb46e9350a6c160a6cc488e
Aldiyar <aissenbayev@one.kz>
2022-10-18 13:48:40 +0600

    Done test8

--- src/main/java/kz/iitu/tests/Test8FooterNavigation.java
+++ src/main/java/kz/iitu/tests/Test8FooterNavigation.java
@@ -1,15 +1,30 @@
 package kz.iitu.tests;
 
 import kz.iitu.Test;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.util.Arrays;
 
 public class Test8FooterNavigation extends Test {
     public Test8FooterNavigation(WebDriver driver) {
         super(driver);
     }
 
+    private static final String[] pages = {"Attachments", "Equipment", "Parts", "Power Systems", "Financing & Insurance",
+            "Maintenance", "Technology & Solutions", "Contact Us", "Find Dealer", "Rental", "Used"};
+
     @Override
     public void execute() {
+        String prevUrl = driver.getCurrentUrl();
+        for (String page : pages) {
+            By openPageSelector = By.linkText(page);
+            driverUtil.clickElement(openPageSelector);
 
+            String newUrl = driver.getCurrentUrl();
+            Assertions.assertNotEquals(newUrl, prevUrl);
+            prevUrl = newUrl;
+        }
     }
 }
